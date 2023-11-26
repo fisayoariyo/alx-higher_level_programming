bedb62847358f98d0386d87f5dbeec4b0cb99753
Adefisayomi <ariyoadefisayomi@gmail.com>
2023-11-26 11:24:26 -0800

    fixed bug

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Module that lists all states from the hbtn_0e_0_usa database."""
+"""
+This script lists all states with
+a `name` starting with the letter `N`
+from the database `hbtn_0e_0_usa`.
+"""
+
 import sys
 import MySQLdb
 
+"""
+Access to the database and get the states
+from the database.
+"""
 if __name__ == "__main__":
-    # Get MySQL credentials from command-line arguments
-    # Connect to MySQL server
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-
-    # Execute the SQL query to retrieve all states sorted by id
-    c.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in c.fetchall() if state[1][0] == "N"]
-
+    db_con = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db_cur = db_con.cursor()
+    db_cur.execute("SELECT * FROM `states` ORDER BY `id`")
+    [print(state) for state in db_cur.fetchall() if state[1][0] == "N"]
