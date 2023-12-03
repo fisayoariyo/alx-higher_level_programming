e07e40d717bc1c183f93028a0722046e36fdfeb9
Adefisayomi <ariyoadefisayomi@gmail.com>
2023-12-03 14:10:30 -0800

    fixed a bug

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a POST request to the passed URL using curl, and display the body of the response
-curl -s -X POST -d "email=testing@gmail.com&subject=I will always be here for PLD" "$1"
+# To send a POST request to the passed URL using curl, and also to display the body of the response
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"
